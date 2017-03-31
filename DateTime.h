8e17e9075fa3a06f65a78bb324d442115f0f6090
Martijn van Buul <martijn.van.buul@gmail.com>
2017-03-31 07:39:59 +0200

    Use time from DS1307

---
+++ DateTime.h
@@ -0,0 +1,18 @@
+#ifndef __DATETIME_H__
+#define __DATETIME_H__
+
+struct DateTime
+{
+  // All in BCD.
+
+  uint8_t sec;
+  uint8_t min; 
+  uint8_t hour;
+  uint8_t wday; // weekday, 1-7
+  uint8_t day;
+  uint8_t month;
+  uint8_t year; // 20xx
+};
+
+extern struct DateTime TheDateTime;
+#endif
--- main.c
+++ main.c
@@ -2,8 +2,138 @@
 #include <util/delay.h>
 #include "TimeRenderer.h"
 #include "Panels.h"
+#include "DateTime.h"
 
 #define TICKTIME 50
+
+struct DateTime TheDateTime;
+
+inline void I2CWait()
+{
+
+  while (!(TWCR & _BV(TWINT)))
+    ; // Wait
+
+}
+
+void Write_DS1307_regs(uint8_t reg, uint8_t amount, uint8_t *ptr)
+{
+  if (amount == 0)
+    return;
+
+  // Send START
+  TWCR = _BV(TWINT)| _BV(TWSTA) | _BV(TWEN);
+
+  I2CWait();
+
+  if ((TWSR & 0xF8) != 0x08)
+    return;
+
+  TWDR=0xd0;  // Slave address, write
+  TWCR = _BV(TWEN) | _BV(TWINT);
+  I2CWait();
+
+  if ((TWSR & 0xf8) != 0x18)
+    goto error; // Not acked
+
+  // Send register address
+
+  TWDR = reg;
+  TWCR = _BV(TWEN) | _BV(TWINT);
+  I2CWait();
+
+  if ((TWSR & 0xf8) != 0x28)
+    goto error; // Not ack.
+
+  while(amount--)
+  {
+    TWDR = *ptr++;
+    TWCR = _BV(TWEN) | _BV(TWINT) | _BV(TWEA); // Clear TWINT to resume,
+    I2CWait();
+    if((TWSR & 0xf8) != 0x28)
+      break;
+  }
+
+error:
+  TWCR = _BV(TWINT) | _BV(TWSTO) | _BV(TWEN); // Stop    
+}
+void Read_DS1307_regs(uint8_t reg, uint8_t amount, uint8_t *ptr)
+{
+  if (amount == 0)
+    return;
+
+  // Send START
+  TWCR = _BV(TWINT)| _BV(TWSTA) | _BV(TWEN);
+
+  I2CWait();
+
+  if ((TWSR & 0xF8) != 0x08)
+    return;
+
+  TWDR=0xd0;  // Slave address, write
+  TWCR = _BV(TWEN) | _BV(TWINT);
+  I2CWait();
+
+  if ((TWSR & 0xf8) != 0x18)
+    goto error; // Not acked
+
+  // Send register address
+
+  TWDR = reg;
+  TWCR = _BV(TWEN) | _BV(TWINT);
+  I2CWait();
+
+  if ((TWSR & 0xf8) != 0x28)
+    goto error; // Not ack.
+
+  // Send repeated start
+  TWCR = _BV(TWINT)| _BV(TWSTA) | _BV(TWEN);
+  I2CWait();
+
+  if ((TWSR & 0xF8) != 0x10)
+    goto error;
+
+  TWDR=0xd1;  // Slave address, read
+  TWCR = _BV(TWEN) | _BV(TWINT);
+  I2CWait();
+
+  if ((TWSR & 0xf8) != 0x40)
+    goto error; // Not acked
+
+  while(--amount)
+  {
+    TWCR = _BV(TWEN) | _BV(TWINT) | _BV(TWEA); // Clear TWINT to resume, send ack to indicate more data is requested
+    I2CWait();
+    if((TWSR & 0xf8) != 0x50)
+      goto error; // Not acked.
+
+    *ptr++ = TWDR;
+  }
+
+  //read last byte
+
+  TWCR = _BV(TWEN) | _BV(TWINT); // Clear TWINT to resume, send nack 
+  I2CWait();
+  *ptr++ = TWDR;
+
+error:
+  TWCR = _BV(TWINT) | _BV(TWSTO) | _BV(TWEN); // Stop    
+}
+
+void Read_DS1307_DateTime()
+{
+  // Read the first 7 registers.
+
+  Read_DS1307_regs(0, 7, (uint8_t *)&TheDateTime);
+  
+  // Check bit 7 of "sec" - if it is 1, the clock wasn't running!
+  if (TheDateTime.sec & 0x80)
+  {
+    TheDateTime.sec &= 0x7f;
+    _delay_ms(100); 
+    Write_DS1307_regs(0,1,(uint8_t *)&TheDateTime); // Clear it
+  }
+}
 
 int main(void)
 {
@@ -13,13 +143,22 @@ int main(void)
 
   // Enable SPI, master mode, 1MHz clock
   SPCR |= _BV(SPE) | _BV(MSTR) | _BV(SPR0);
- 
+
+  // Set up I2C
+  
+  DDRC = 0; // entire port input
+  PORTC = 0x3f; // Enable pull-up
+  DDRC = 0x30; // PC4 and PC5 to output.
+
+  // Configure I2C speed: 100 KHz @ 16MHz clock
+  TWSR = 0;
+  TWBR = 72;
+  TWCR = _BV(TWEN); // enable I2C
+
   InitializePanels(3);
   SetBrightness(1);
 
-  uint8_t hours = 12, mins = 34;
-
-  TimeRenderer_SetTime(hours,mins,false); // reset
+  TimeRenderer_SetTime(99,99,false); // reset
   
   uint8_t trig = 0;
   
@@ -30,14 +169,9 @@ int main(void)
     trig = trig + 1;
     if (trig == (1000 / TICKTIME) )
     {
+      Read_DS1307_DateTime();
+      TimeRenderer_SetTime(TheDateTime.min,TheDateTime.sec,true); 
       trig = 0;
-      mins = mins + 1;
-      if (mins == 60)
-      {
-	mins = 0;
-	hours++;
-      }
-      TimeRenderer_SetTime(hours,mins,true); 
     }
   }
 }
